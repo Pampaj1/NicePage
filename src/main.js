acc81b2eededde4eb7e6a4c52c1b429f4894dcae
Pampaj <dawidkawinski@opoczta.pl>
2023-10-11 19:41:19 +0200

    add a lot of new things

--- src/main.js
+++ src/main.js
@@ -1,5 +1,24 @@
+const inputBox = document.getElementById('input-box')
+const taskContainer = document.querySelector('task-container')
 const addBtn = document.querySelector('.fa-circle-plus')
 const addBar = document.querySelector('.row')
+const listElement = document.querySelector('.list-element')
+const squareChecked = document.querySelector('.fa-square')
+
+
+
+
+function addTask () {
+    if(inputBox.value === '') {
+        alert('error')
+    } else {
+        let li = document.createElement("li")
+        li.innerHTML = inputBox.value
+        taskContainer.appendChild(li)
+    }
+}
+
+
 
 addBtn.addEventListener('click', () => {
     addBar.classList.toggle('show')
@@ -7,3 +26,6 @@ addBtn.addEventListener('click', () => {
     addBtn.classList.toggle('fa-circle-plus')
 })
 
+listElement.addEventListener('click', () => {
+    listElement.classList.toggle('checked')
+})
