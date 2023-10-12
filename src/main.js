a3dba853b972fe36c9ffde9ea0938c67d75cdf71
Pampaj <dawidkawinski@opoczta.pl>
2023-10-12 18:14:01 +0200

    still i didn't fix bug with adding a new tasks

--- src/main.js
+++ src/main.js
@@ -1,44 +1,36 @@
 //Input element with which you can add new tasks
-const inputBox = document.getElementById('input-box')
+const inputBox = document.getElementById('.input-box')
 const addBar = document.querySelector('.row')
+
 
 //Button for input bar that drop 
 const addBtn = document.querySelector('.fa-circle-plus')
 
 //Elements that are responsible for the list with tasks
-const taskContainer = document.querySelector('task-container')
-const task = document.querySelector('list-element')
+const taskContainer = document.querySelector('.task-container')
+const task = document.querySelector('.list-element')
 
-/*
-function addTask () {
-    if (inputBox.value === '') {
-        alert('error')
-    } else {
-        let li = document.createElement("li");
-        li.innerHTML = inputBox.value;
-        taskContainer.appendChild(li);
-    }
-}
+
+
 
 task.addEventListener('click', () => {
     task.classList.toggle('checked')
 })
-
 
 addBtn.addEventListener('click', () => {
     addBar.classList.toggle('show')
     addBtn.classList.toggle('fa-circle-minus')
     addBtn.classList.toggle('fa-circle-plus')
 })
-*/
+
 
 function addTask () {
-    if (inputBox.value === '') {
+    if (inputBox.value === '') {    
         alert('error')
     } else {
-        let li = document.createElement('li')
+        const li = document.createElement("li")
         li.innerHTML = inputBox.value
         taskContainer.appendChild(li)
     }
-    inputBox.value = ""
+    inputBox.value = ''
 }
